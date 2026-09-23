bf611dedc377a63b11c1da96675da779a17482cd
sanskardixitt <sanskardixit81@gmail.com>
2026-09-23 07:15:37 +0530

    palindrome problem

---
+++ Dsa problem solved/palindrome.js
@@ -0,0 +1,18 @@
+function palindrome(num){
+
+    let rev = 0;
+    let temp =  num;
+     if (num < 0 || (num !== 0 && num % 10 === 0)) return false; 
+    while(num>0){
+
+        let x =num%10;
+        rev = rev*10+x;
+        num = Math.floor(num/10);
+
+    }
+    if(rev == temp){
+        return true;
+    }
+    return false;
+}
+console.log(palindrome(12321));
